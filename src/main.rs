d9b23c4ac07fd717d06d6ef3ee0b3e5aaffc6445
Angelo Puglisi <angelopuglisi86@gmail.com>
2021-12-14 22:29:06 +0100

    feat: check args
    Signed-off-by: Angelo Puglisi <angelopuglisi86@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let param: Vec<String> = std::env::args().collect();
+
+    if param.len() < 2 {
+        println!("Missing argument");
+        std::process::exit(1);
+    }
+    let param = &param[1];
+
+    let file: elf::File;
+    {
+        let path = std::path::PathBuf::from(param);
+        file = elf::File::open_path(&path).unwrap_or_else(|e| panic!("Error: {:?}", e));
+    }
+
+    let data = file
+        .get_section(".interp")
+        .expect("Failed to look up .interp section")
+        .data
+        .as_slice();
+
+    // skip the trailing \0
+    std::io::stdout()
+        .write_all(&data[..data.len() - 1])
+        .unwrap();
+    println!();
 }
